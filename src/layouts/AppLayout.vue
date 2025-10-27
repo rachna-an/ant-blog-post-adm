524dedd614e384937b41afb9d51c19fa1b279492
Rachna-An <anrachna11@gmail.com>
2025-10-27 10:22:21 +0700

    Initial commit: setup Vue + Tailwind project

---
+++ src/layouts/AppLayout.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="bg-white drawer lg:drawer-open" id="drawer">
+    <input id="my-drawer" type="checkbox" class="drawer-toggle" />
+
+    <div class="drawer-content h-full! bg-[#F8F9FC]">
+      <!-- <DashboardNavbar /> -->
+
+      <!-- Page content  -->
+      <main class="p-6! pt-4!">
+        <RouterView />
+      </main>
+    </div>
+
+    <!-- <DashboardSidebar /> -->
+  </div>
+</template>
+
+<script setup>
+  //   import DashboardNavbar from '@/components/dashboard/DashboardNavbar.vue'
+  //   import DashboardSidebar from '@/components/dashboard/DashboardSidebar.vue'
+</script>
+
+<style scoped>
+  .drawer-content {
+    min-height: 100vh;
+  }
+</style>
